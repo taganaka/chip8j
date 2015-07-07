644a50cea029a4beffaf3bd0eb036e00b8f0f878
taganaka <taganaka@gmail.com>
2015-07-07 00:06:46 -0700

    initialize registers to 0

--- src/chip/Chip.java
+++ src/chip/Chip.java
@@ -69,8 +69,10 @@ public class Chip {
         rnd = new Random();
         clearScreen();
 
-        for (int i = 0; i < keys.length; i++)
+        for (int i = 0; i < keys.length; i++){
             keys[i] = 0;
+            V[i]    = 0;
+        }
     }
 
     public void run(){
